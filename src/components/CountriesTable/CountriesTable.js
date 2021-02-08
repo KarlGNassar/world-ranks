68e92d8e6d2ddbaa4df8d8fa96cf395a70b8fa0e
KARL NASSAR <kgnassar01@gmail.com>
2021-02-08 12:45:21 +0200

    first commit

---
+++ src/components/CountriesTable/CountriesTable.js
@@ -0,0 +1,111 @@
+import styles from './CountriesTable.module.css'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
+import { useState } from 'react'
+import Link from 'next/link'
+
+const orderBy = (countries, value, direction) => {
+    if (direction === 'asc'){
+        return [...countries].sort((a, b) => a[value] > b[value] ? 1 : -1)
+    }
+
+    if (direction === 'desc') {
+        return [...countries].sort((a, b) => a[value] > b[value] ? -1 : 1)
+    }
+
+    return countries
+}
+
+const SortArrow = ({direction}) => {
+    if (!direction) {
+        return <></>
+    }
+    
+    if (direction === 'desc') {
+        return (
+            <div className={styles.heading_arrow}>
+                <KeyboardArrowDownIcon color="inherit" />
+            </div>
+        )
+    } else {
+        return (
+            <div className={styles.heading_arrow}>
+                <KeyboardArrowUpIcon color="inherit" />
+            </div>
+        )
+    }
+}
+
+const CountriesTable = ({countries}) => {
+    const [direction, setDirection] = useState('desc')
+    const [value, setValue] = useState('population')
+
+    const orderedCountries = orderBy(countries, value, direction)
+
+    const switchDirection = () => {
+        if (!direction) {
+            setDirection('desc')
+        } else if (direction === 'desc') {
+            setDirection('asc')
+        } else {
+            setDirection(null)
+        }
+    }
+
+    const setValueDirection = value => {
+        switchDirection()
+        setValue(value)
+    }
+
+    return (
+        <div>
+            <div className={styles.heading}>
+                <div className={styles.heading_flag}></div>
+                <button className={styles.heading_name} onClick={() => setValueDirection('name')}>
+                    <div>Name</div>
+                    {value === 'name' && <SortArrow direction={direction}/>}
+                </button>
+
+                <button className={styles.heading_population} onClick={() => setValueDirection('population')}>
+                    <div>Population</div>
+                    {value === 'population' &&<SortArrow direction={direction} />}
+                </button>
+
+                <button className={styles.heading_area} onClick={() => setValueDirection('area')}>
+                    <div>Area (km <sup style={{fontSize: '0.5rem'}}>2</sup>)</div>
+                    {value === 'area' && <SortArrow direction={direction} />}
+                </button>
+
+                <button className={styles.heading_gini} onClick={() => setValueDirection('gini')}>
+                    <div>Gini</div>
+                    {value === 'gini' && <SortArrow direction={direction} />}
+                </button>
+            </div>
+
+            {orderedCountries.map(country => (
+                <Link href={`country/${country.alpha3Code}`} key={country.alpha2Code}>
+                    <div className={styles.row}>
+                        <div className={styles.flag}>
+                            <img src={country.flag} alt={country.name}/>
+                        </div>
+                        <div className={styles.name}>
+                            {country.name}
+                        </div>
+
+                        <div className={styles.population}>
+                            {country.population}
+                        </div>
+                        <div className={styles.area}>
+                            {country.area || 0}
+                        </div>
+                        <div className={styles.gini}>
+                            {country.gini || 0} %
+                        </div>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    )
+}
+
+export default CountriesTable
